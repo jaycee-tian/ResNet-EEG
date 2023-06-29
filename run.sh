b0f196a2ba4d0f86a41fa07eded712c4ea490289
sleep-tian <junchao.tian@outlook.com>
2023-06-29 11:41:56 +0800

    a

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 # 获得gpu的编号变量
 . script/prepare.sh
 
-nohup python -u main.py --pretrain --model_name ${model_name1} > ${log_dir}/${model_name1}.log &
+nohup python -u main.py  --model_name ${model_name1} > ${log_dir}/${model_name1}.log &
 sleep 5
-nohup python -u main.py --pretrain --model_name ${model_name2} > ${log_dir}/${model_name2}.log &
+nohup python -u main.py  --model_name ${model_name2} > ${log_dir}/${model_name2}.log &
 sleep 5
-nohup python -u main.py --pretrain --model_name ${model_name3} > ${log_dir}/${model_name3}.log &
+nohup python -u main.py  --model_name ${model_name3} > ${log_dir}/${model_name3}.log &
 sleep 5
-nohup python -u main.py --pretrain --model_name ${model_name4} > ${log_dir}/${model_name4}.log &
+nohup python -u main.py  --model_name ${model_name4} > ${log_dir}/${model_name4}.log &
 sleep 5
-nohup python -u main.py --pretrain --model_name ${model_name5} > ${log_dir}/${model_name5}.log &
+nohup python -u main.py  --model_name ${model_name5} > ${log_dir}/${model_name5}.log &
